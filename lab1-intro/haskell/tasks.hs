7455bb21a6b8d0929a048d115553428c726d4d11
Kazi Arham <210149902@aston.ac.uk>
2023-04-15 10:16:46 +0000

    Completed code for Lab 1

--- lab1-intro/haskell/tasks.hs
+++ lab1-intro/haskell/tasks.hs
@@ -1,13 +1,13 @@
 -- The following function definition contains a syntax error:
 -- (To enable the code, delete contents of lines 10 and 3 and line -- on line 24)
-{-
+
 sayHello :: IO ()
 sayHello =
   do
   let name = "Alice"
-   putStrLn "hello"
+  putStrLn "hello"
   putStrLn name
--}
+
 
 ask :: String -> IO ()
 ask prompt =
@@ -15,11 +15,21 @@ ask prompt =
   putStrLn prompt
   line <- getLine
   if line == ""
-    then ask prompt
+    -- then ask prompt
+    then ask (prompt ++ "!") -- Task c-iii - when someone presses enter without typing anything, the repeated prompt will have an extra "!" at the 
+    -- ask (1 + prompt) -- Task c-iv - compiler error
+  else if line == "quit"
+    then putStrLn ("quitting...") -- Task c ii - if quit is entered, the message "quitting" is displayed
     else putStrLn ("you said: " ++ reverse line)
+
+-- Task a - define function called myFunc and takes one argument
+myFunc :: Integer -> Integer
+myFunc x = x*10
 
 main :: IO ()
 main =
   do
-  --sayHello
+  sayHello
   ask "please say something"
+  print (myFunc 3) -- Call the myFunc function
+  print (show(myFunc 3)) -- Task b - Convert integer to string
--- lab1-intro/haskell/tasks2.hs
+++ lab1-intro/haskell/tasks2.hs
@@ -1,16 +1,26 @@
 
---Code for P1-T-d-i
+{- --Code for P1-T-d-i
+onePlusone :: IO()
+onePlusone =
+    do
+    putStrLn("1 + 1 = 2")
+
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    onePlusone
+-}
 
-{- --Code for P1-T-d-ii
-sgn x = if x < 0 then -1 else if x == 0 then 0 else 1
+ --Code for P1-T-d-ii
+-- sgn x = if x < 0 then -1 else if x == 0 then 0 else 1
+-- Task d-iii - Implement the guards on sgn function
+sgn x | x < 0 = -1 -- first guard
+    | x == 0 = 0 -- second guard
+    | x > 0 = 1 -- third guard
 
 main = do
     putStrLn "Please enter a number"
     input <- getLine 
     let x = (read input :: Int)
     putStrLn( show (sgn (x)) )
--}
